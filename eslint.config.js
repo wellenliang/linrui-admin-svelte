62994c8a9e8084d83459c4e4a3cda06fc91970c4
wellenliang <wellenliang@163.com>
2024-04-13 22:45:41 +0800

    项目初始化

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import antfu from "@antfu/eslint-config";
+
+export default antfu({
+  stylistic: {
+    indent: 2,
+    quotes: "single",
+    semi: true,
+  },
+  typescript: true,
+  svelte: true,
+});
